435799d3c1e764f4e5d4e0a73df46ea4e4672a0c
zhangxk <zhangxk25@163.com>
2019-11-14 15:54:51 +0800

    修改规则

--- steve.js
+++ steve.js
@@ -1,4 +1,4 @@
-const doList = [['腹', '腿'], ['胸', '臂']];
+const doList = ['腹', '腿', '胸', '臂'];
 
 const now = new Date();
 const year = now.getFullYear();
@@ -11,9 +11,7 @@ const theFirstDay_time = +new Date(year, 0);
 const today_time = new Date(year, month, day);
 const days = (today_time - theFirstDay_time) / 1000 / 60 / 60 / 24;
 
-const index = days % doList.length;
-const tody_do = doList[index];
-const now_do = tody_do[ampm === 'am' ? 0 : 1];
+const now_do = doList[days % doList.length]
 
 const $content = document.querySelector('.content');
 $content.innerHTML = now_do;
@@ -23,20 +21,21 @@ $time.innerHTML = now.toLocaleString();
 setInterval(() => {
   const now = new Date();
   $time.innerHTML = now.toLocaleString();
-}, 1000)
-
+}, 1000);
 
 function resetFontSize() {
   let d = document.createElement('div');
-  d.style.cssText = "width:1rem;height:0;overflow: hidden;position:absolute;z-index:-1;visibility: hidden;";
+  d.style.cssText =
+    'width:1rem;height:0;overflow: hidden;position:absolute;z-index:-1;visibility: hidden;';
   document.body.appendChild(d);
   let dw = d.offsetWidth; // 1rem的实际展示px值
   document.body.removeChild(d);
   let html = document.querySelector('html');
   if (!!html) {
     let fz = html.style.fontSize.slice(0, -2) * 1 || 0; //正常计算出来的rem基准值 , 可自行修改为rem计算好的值
-    let realRem = fz
-    if (dw != fz) { //不相等 则被缩放了
+    let realRem = fz;
+    if (dw != fz) {
+      //不相等 则被缩放了
       realRem = Math.pow(fz, 2) / dw;
     }
     html.style.fontSize = realRem + 'px';
@@ -46,7 +45,7 @@ function resetFontSize() {
 function autoRem() {
   let docEl = document.documentElement,
     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-    recalc = function () {
+    recalc = function() {
       let clientHeight = docEl.clientHeight;
       let clientWidth = docEl.clientWidth;
       if (!clientHeight) return;
@@ -54,7 +53,7 @@ function autoRem() {
       if (wh > 16 / 9) {
         docEl.style.fontSize = 100 * (clientHeight / 720) + 'px';
       } else {
-        docEl.style.fontSize = 100 * (clientWidth * 9 / (720 * 16)) + 'px';
+        docEl.style.fontSize = 100 * ((clientWidth * 9) / (720 * 16)) + 'px';
       }
       resetFontSize();
     };
